3e904b9283895a6e03412a4ec85778e69d501fda
likhithamajji <majjilikhitha77@gmail.com>
2025-03-01 19:09:59 +0530

    Add files via upload

---
+++ StrUpperandLower.java
@@ -0,0 +1,9 @@
+public class StrUpperandLower {
+    public static void main(String[] args) {
+        String s = "divya";
+        System.out.println(s.toUpperCase());
+        String k ="LIKHITHA";
+        System.out.println(k.toLowerCase());
+    }
+    
+}
